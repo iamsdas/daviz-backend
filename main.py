288bca6e4ec896b77368055f646e91253070eda9
Suryashanar Das <suryashankardas.2002@gmail.com>
2022-12-03 00:34:56 +0530

    add time series data option

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from io import StringIO
+
+import pandas as pd
+from fastapi import FastAPI, UploadFile, Form
+
+from utils import parse_time_series_data
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.post("/api")
+def get_config(
+    csv_file: UploadFile,
+    x_axis: str | None = Form(default=None),
+    y_axes: list[str] | None = Form(default=None),
+):
+    df = pd.read_csv(StringIO(str(csv_file.file.read(), "utf-8")), encoding="utf-8")
+    return parse_time_series_data(df, x_axis, y_axes)
