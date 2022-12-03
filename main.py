cd691134c22d03da48a8d08f9a83884c2a816b37
Suryashanar Das <suryashankardas.2002@gmail.com>
2022-12-03 22:05:58 +0530

    add different data types

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from io import StringIO
 import pandas as pd
 from fastapi import FastAPI, UploadFile, Form
 
-from utils import parse_time_series_data
+from utils import parse_continuous_data, parse_categorical_data, parse_discrete_data
 
 app = FastAPI()
 
@@ -16,6 +16,17 @@ def get_config(
     y_axes: list[str] | None = Form(default=None),
 ):
     df = pd.read_csv(StringIO(str(csv_file.file.read(), "utf-8")), encoding="utf-8")
-    if data_type == "time_series":
-        return parse_time_series_data(df, x_axis, y_axes)
+    df.dropna(inplace=True)
+    if x_axis is None or x_axis == "" or y_axes is None or y_axes == []:
+        x_axis = df.columns[0]
+        y_axes = df.columns[1:]
+    df.sort_values(by=x_axis, inplace=True)
+
+    if data_type == "continuous":
+        return parse_continuous_data(df, x_axis, y_axes)
+    if data_type == "discrete":
+        return parse_discrete_data(df, x_axis, y_axes)
+    if data_type == "categorical":
+        return parse_categorical_data(df, x_axis, y_axes)
+
     return "invalid data type"
--- utils.py
+++ utils.py
@@ -1,16 +1,38 @@
 import pandas as pd
 
 
-def parse_time_series_data(
+def parse_continuous_data(
     df: pd.DataFrame, x_axis: str | None = None, y_axes: list[str] | None = None
 ):
-    x_axis = x_axis or df.columns[0]
-    y_axes = y_axes or df.columns[1:]
 
-    df.dropna(inplace=True)
-    df.sort_values(by=x_axis, inplace=True)
     labels = df[x_axis].values
 
+    return {
+        "labels": list(labels),
+        "datasets": [
+            {"data": list(df[column].values), "label": labels[index]}
+            for index, column in enumerate(y_axes)
+        ],
+    }
+
+
+def parse_discrete_data(
+    df: pd.DataFrame, x_axis: str | None = None, y_axes: list[str] | None = None
+):
+    labels = df[x_axis].values
+    return {
+        "labels": list(labels),
+        "datasets": [
+            {"data": list(df[column].values), "label": labels[index]}
+            for index, column in enumerate(y_axes)
+        ],
+    }
+
+
+def parse_categorical_data(
+    df: pd.DataFrame, x_axis: str | None = None, y_axes: list[str] | None = None
+):
+    labels = df[x_axis].values
     return {
         "labels": list(labels),
         "datasets": [
